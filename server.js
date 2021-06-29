23badc41de71bacab99cb12d1e4be24162342354
Sai Alankrutha Pandra Pagada <alankrutha236@gmail.com>
2021-06-29 14:30:34 -0400

    added npm

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require ('path');
 const app = express ();
+// include and initialize the rollbar library with your access token(copyied)
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: '4656e99901f74303ab58e5ab004444ed',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
 
 app.use(express.json())
 
 
 app.get('/',function(req,res){
+    rollbar.log('Hello World')
     res.sendFile(path.join(__dirname,'/public/index.html'))
 })
 
